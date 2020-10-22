9bbaa5730239f9b74c5d8a6d5477c90c7c31a2fb
Sarath0392 <59784953+Sarath0392@users.noreply.github.com>
2020-10-22 18:43:53 +0530

    Initial commit

---
+++ src/main/java/com/logicinfo/EmailAlertProject/EmailAlertServlet.java
@@ -0,0 +1,43 @@
+package com.logicinfo.EmailAlertProject;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmailAlertServlet
+ */
+@WebServlet("/EmailAlertServlet")
+public class EmailAlertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmailAlertServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	    response.setContentType("text/plain");
+	    response.getWriter().println("Hello App Engine");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
